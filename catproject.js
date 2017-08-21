ee3a424677db05fa847724a6999a37090d7e2808
Christopher Aubuchon <aubuchcl@gmail.com>
2017-08-21 01:41:08 -0700

    added a counter and second cat

--- catproject.js
+++ catproject.js
@@ -1,10 +1,20 @@
 // console.log("hello")
-var counterId = document.getElementById('counter')
+var counterId = document.getElementById('counter');
 var catClicks = document.getElementById('cat');
-var count = 0
+var count = 0;
 catClicks.addEventListener('click', function(){
 	// add counter
-	count += 1
-	counterId.innerHTML = "Click count: " + count
+	count += 1;
+	counterId.innerHTML = "Click count: " + count;
 
-}, false)
+}, false);
+
+var counterId2 = document.getElementById('counter2');
+var catClicks2 = document.getElementById('cat2');
+var count2 = 0;
+catClicks2.addEventListener('click', function(){
+	// add counter
+	count2 += 1;
+	counterId2.innerHTML = "Click count: " + count2;
+
+}, false);
