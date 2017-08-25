0be2e8c881ec75ee51939f1e5e0db4c2980b98eb
Christopher Aubuchon <aubuchcl@gmail.com>
2017-08-24 18:47:55 -0700

    no changes .idea file from pycharm

--- catproject.js
+++ catproject.js
@@ -1,37 +1,37 @@
 var catPics = ['cat.jpg', 'cat2.jpg', 'cat3.jpg', 'cat4.jpg', 'cat5.jpg']
 
-for(var i = 0; i < catPics.length; i++){
-	var cat = catPics[i];
+for (var i = 0; i < catPics.length; i++) {
+    var cat = catPics[i];
 
 
-	var figHolder = document.createElement('figure')
-	// make the count p element and cat img element
-	var catElement = document.createElement('img');
-	var countElement = document.createElement('figcatption');
-	document.body.appendChild(figHolder)
-	figHolder.appendChild(catElement)
-	figHolder.appendChild(countElement)
+    var figHolder = document.createElement('figure')
+    // make the count p element and cat img element
+    var catElement = document.createElement('img');
+    var countElement = document.createElement('figcaption');
+    document.body.appendChild(figHolder)
+    figHolder.appendChild(catElement)
+    figHolder.appendChild(countElement)
 
-	//strip off file name and use it as id
-	var catNoExtension = cat.substring(0, cat.indexOf('.'));
+    //strip off file name and use it as id
+    var catNoExtension = cat.substring(0, cat.indexOf('.'));
 
-	// set id to newly created img element and then set src
-	catElement.id = catNoExtension;
-	catElement.src = cat;
+    // set id to newly created img element and then set src
+    catElement.id = catNoExtension;
+    catElement.src = cat;
 
-	countElement.id = "count"+i
+    countElement.id = "count" + i
 
-	var count = 0
-	catElement.addEventListener('click',
-		                       (function(numCount, numId){
-		return function(){
-			numCount += 1
-			var x = document.getElementById("count"+numId)
-			x.innerHTML = "This picture has been clicked <em>"
-			               + numCount +
-			               "</em> times!!!"
-		};
-	})(count, i));
+    var count = 0
+    catElement.addEventListener('click',
+        (function (numCount, numId) {
+            return function () {
+                numCount += 1
+                var x = document.getElementById("count" + numId)
+                x.innerHTML = "This picture has been clicked <em>"
+                    + numCount +
+                    "</em> times!!!"
+            };
+        })(count, i));
 }
 
 
